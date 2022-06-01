09c514fdd21afdf2b3c21137978d1da4dc606905
imjacky <jacky19941214@gmail.com>
2022-06-02 01:46:12 +0800

    Layout & Activity - Create new activity - Detail page

---
+++ app/src/main/java/com/example/mobile_217014620/DetailListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobile_217014620
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail_page_layout)
+    }
+}
