3e801f1ac8f2cf8ba3f278039036d11e054bed1a
imjacky <jacky19941214@gmail.com>
2022-06-03 21:53:39 +0800

    Activity - change loading data from firebase

--- app/src/main/java/com/example/mobile_217014620/ShopListActivity.kt
+++ app/src/main/java/com/example/mobile_217014620/ShopListActivity.kt
@@ -3,23 +3,58 @@ package com.example.mobile_217014620
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 data class Shop(val name : String, val location : String, val category: String, val photo : Int)
 
 class ShopListActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page_layout)
 
-        var shopName = resources.getStringArray(R.array.shopName)
-        var shopLocation = resources.getStringArray(R.array.shopLocation)
-        var shopCategory = resources.getStringArray(R.array.shopCategory)
-        var shopPhoto: Array<Int> = arrayOf(
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("shopList")
+
+        val nameList: MutableList<String?> = ArrayList()
+        val locationList: MutableList<String?> = ArrayList()
+        val categoryList: MutableList<String?> = ArrayList()
+        val imgList: MutableList<String?> = ArrayList()
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (ds in dataSnapshot.children) {
+                    val name = ds.child("name").getValue(String::class.java)
+                    val location = ds.child("location").getValue(String::class.java)
+                    val category = ds.child("category").getValue(String::class.java)
+                    val img = ds.child("img").getValue(String::class.java)
+                    nameList.add(name)
+                    locationList.add(location)
+                    categoryList.add(category)
+                    imgList.add(img)
+                }
+                step2(nameList, locationList, categoryList)
+            }
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("DetailListActivity", "Failed to read value.", error.toException())
+            }
+        })
+    }
+
+    fun step2(nameList: MutableList<String?>, locationList: MutableList<String?>, categoryList: MutableList<String?>){
+        val shopPhoto: Array<Int> = arrayOf(
             R.drawable.a1,
             R.drawable.b1,
             R.drawable.c1,
@@ -29,8 +64,8 @@ class ShopListActivity : AppCompatActivity() {
         var shopList = ArrayList<Shop>()
 
         @Override
-        for (i in 0 until shopName.size) {
-            val s = Shop(shopName[i], shopLocation[i], shopCategory[i], shopPhoto[i])
+        for (i in 0 until nameList.size) {
+            val s = Shop(""+nameList[i], ""+locationList[i], ""+categoryList[i], shopPhoto[i])
             shopList.add(s)
         }
 
@@ -73,3 +108,4 @@ class ShopAdapter(context: Context, resource: Int, objects: MutableList<Shop>) :
         return v!!
     }
 }
+
