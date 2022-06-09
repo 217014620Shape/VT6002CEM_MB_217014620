5cecdb175029873435fe29a081a3ac6b12c5923b
imjacky <jacky19941214@gmail.com>
2022-06-10 03:42:07 +0800

    Activity - add img Upload function

--- app/src/main/java/com/example/mobile_217014620/CameraActivity.kt
+++ app/src/main/java/com/example/mobile_217014620/CameraActivity.kt
@@ -8,44 +8,42 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.tasks.Continuation
-import com.google.android.gms.tasks.Task
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.UploadTask
 import java.io.IOException
 import java.util.*
 
 class CameraActivity : AppCompatActivity() {
-
-    private lateinit var takePhoto: Button
-    private lateinit var upload: Button
-    private lateinit var photo: ImageView
-    private val cameraRequestId = 200
-
     private val PICK_IMAGE_REQUEST = 71
+    private val cameraRequestId = 200
     private var filePath: Uri? = null
     private var firebaseStore: FirebaseStorage? = null
     private var storageReference: StorageReference? = null
+    lateinit var imagePreview: ImageView
+    lateinit var btn_choose_image: Button
+    lateinit var btn_upload_image: Button
+    lateinit var takePhoto: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.camera_page_layout)
-
-        assert(supportActionBar != null)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-
+        btn_choose_image = findViewById(R.id.takeGallery)
+        btn_upload_image = findViewById(R.id.upload)
+        imagePreview = findViewById(R.id.photo)
         takePhoto = findViewById(R.id.takePhoto)
-        upload = findViewById(R.id.upload)
-        photo = findViewById(R.id.photo)
+
+        firebaseStore = FirebaseStorage.getInstance()
+        storageReference = FirebaseStorage.getInstance().reference
+
+        btn_choose_image.setOnClickListener { launchGallery() }
+        btn_upload_image.setOnClickListener { uploadImage() }
 
         /**get Permission*/
         if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED)
@@ -55,21 +53,43 @@ class CameraActivity : AppCompatActivity() {
             val cameraInt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(cameraInt, cameraRequestId)
         }
+    }
 
-        upload.setOnClickListener {
-            Toast.makeText(this, "Upload to firebase is coming soon", Toast.LENGTH_LONG).show()
-        }
+    private fun launchGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
     }
-    override fun onSupportNavigateUp(): Boolean {
-        finish()
-        return true
-    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            if (data == null || data.data == null) {
+                return
+            }
+            filePath = data.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
+                imagePreview.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
         if (requestCode == cameraRequestId) {
             /**save to Image In layout*/
             val images: Bitmap = data?.extras?.get("data") as Bitmap
-            photo.setImageBitmap(images)
+            imagePreview.setImageBitmap(images)
+        }
+    }
+
+    private fun uploadImage() {
+        if (filePath != null) {
+            val ref = storageReference?.child("myImages/" + UUID.randomUUID().toString())
+            val uploadTask = ref?.putFile(filePath!!)
+
+        } else {
+            Toast.makeText(this, "Please Upload an Image", Toast.LENGTH_SHORT).show()
         }
     }
 
